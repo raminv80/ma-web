6b22a673bd385e74994274f3a6e66b64bab958e8
njspragg <nick@them.com.au>
2013-11-13 12:42:05 +1030

    Add plugin which can product a shrunk + cropped image and also just a shrunk image to reduce the site size.
    Added a plugin to rotate image X degrees. Standard is 90 degrees.
    
    Added to smarty plugins to product shrunk version of image and return
    new link, and one to produce cropped version.
    
    Signed-off-by: njspragg <nick@them.com.au>

---
+++ includes/plugins/rotateimage.php
@@ -0,0 +1,22 @@
+<?php
+if($_SERVER['REMOTE_ADDR'] == '150.101.230.130' || $_SERVER['REMOTE_ADDR'] == '202.6.152.24'){
+	
+	if(empty($_POST['image'])){
+		die('No image.');
+	}
+	include 'rotate.image.90.class.php';
+	try{
+		$rotate = new Rotate_Image_90;
+		$rotate->source_image = 'medium/'.$_POST['image'];
+		$process = $rotate->rotate(-90);
+		$rotate->source_image = 'thumbs/'.$_POST['image'];
+		$process = $rotate->rotate(-90);
+		/* left, center or right; If none is set, center will be used as default */
+	}catch(Exception $e){
+		echo 'Exception throw: '.$e.'<br/>';
+		continue;
+	}
+}
+
+
+
---
+++ plugins/function.img_shrink.php
@@ -0,0 +1,43 @@
+<?php 
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * File:	function.img_shrink.php
+ * Type:	function
+ * Name:	thumbnail
+ * Purpose:	create shrink image version of the image
+ * -------------------------------------------------------------
+ */
+include '/includes/plugins/crop.image.to.square.class.php';
+
+function smarty_function_img_shrink($params, &$smarty)
+{
+	$src = $_SERVER['DOCUMENT_ROOT'];
+	if(empty($params['data'])){
+		return '';
+	}
+	try {
+		$fname = basename($params['data']);
+		if( !file_exists("/images/medium/{$fname}") ){
+			try{
+				if (!file_exists("/images/thumbs")) {
+					mkdir("/images/medium", 0777, true);
+				}
+				$crop = new Crop_Image_To_Square;
+				$crop->source_image = $params['data'];
+				//$crop->save_to_folder = "/images/thumbs";
+				$crop->save_to_folderm = "/images/medium";
+				/* left, center or right; If none is set, center will be used as default */
+				$process = $crop->crop('center',$params['size']);
+			}catch(Exception $e){
+				return '';
+			}
+		}
+	}
+	catch(Exception $e) {
+		return '';
+	}
+    
+    return "/images/medium/{$fname}";
+}
+?>
