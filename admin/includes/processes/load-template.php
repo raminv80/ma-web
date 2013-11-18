dd0287c74ef3cd5e9239d715a9cecaee7deb4219
njspragg <nick@them.com.au>
2013-11-19 08:53:16 +1030

    Signed-off-by: njspragg <nick@them.com.au>

---
+++ admin/includes/processes/load-template.php
@@ -0,0 +1,13 @@
+<?php
+set_include_path ( $_SERVER ['DOCUMENT_ROOT'] );
+include_once 'admin/includes/functions/admin-functions.php';
+global $SMARTY;
+
+$tpl = $_POST['template'];
+if(!file_exists("{$SMARTY->template_dir[0]}".$tpl)){
+	die();
+}
+foreach($_POST as $key => $var){
+	$SMARTY->assign("{$key}",$var);
+}
+$SMARTY->display("{$tpl}");
