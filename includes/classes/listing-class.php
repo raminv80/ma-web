7473cd0189a36ae9328341125dcdb350e101deb0
njspragg <nick@them.com.au>
2015-11-12 15:02:45 +1030

    Signed-off-by: njspragg <nick@them.com.au>

--- includes/classes/listing-class.php
+++ includes/classes/listing-class.php
@@ -271,49 +271,7 @@ class ListClass {
     }
     return false;
   }
-
-  function BuildCache() {
-    global $SMARTY,$DBobject;
-    $sql[0] = "CREATE TABLE IF NOT EXISTS `cache_tbl_listing` (
-		`cache_id` INT(11) NOT NULL AUTO_INCREMENT,
-		`cache_record_id` INT(11) DEFAULT NULL,
-		`cache_url` VARCHAR(255) DEFAULT NULL,
-		`cache_created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-		`cache_modified` DATETIME DEFAULT NULL,
-		`cache_deleted` DATETIME DEFAULT NULL,
-		PRIMARY KEY (`cache_id`)
-		) ENGINE=MYISAM DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;";
-    $DBobject->wrappedSql($sql[0]);
-    
-    $sql[3] = "TRUNCATE cache_tbl_listing;";
-    $sql[2] = "INSERT INTO cache_tbl_listing (cache_record_id,cache_published,cache_url,cache_modified) VALUES  ";
-    $params = array();
-    $sql[1] = "SELECT listing_object_id AS id,listing_published FROM tbl_listing WHERE listing_deleted IS NULL";
-    $res = $DBobject->wrappedSql($sql[1]);
-    $n = 1;
-    
-    foreach($res as $row){
-      $id = $row['id'];
-      $url = "";
-      $published = $row['listing_published'];
-      if(! self::BuildUrl($id,$url,$published)){
-        continue;
-      }
-      $sql[2] .= " ( :id{$n}, :published{$n}, :title{$n}, now() ) ,";
-      $params = array_merge($params,array(
-          ":id{$n}"=>$id,
-          ":published{$n}"=>$published,
-          ":title{$n}"=>$url
-      ));
-      $n ++;
-    }
-    
-    $sql[2] = trim(trim($sql[2]),',');
-    $sql[2] .= ";";
-    $DBobject->wrappedSql($sql[3]);
-    $DBobject->wrappedSql($sql[2],$params);
-  }
-  
+   
   // TODO: THIS NEEDS TO BE REVISED TO SUPPORT HAVING 2 URLS FOR THE SAME LISTING_OBJECT_ID IN THE EVENT THAT THE URL VALUE IS CHANGING.
   function BuildUrl($_id, &$url, $_PUBLISHED = 1) {
     global $DBobject;
@@ -335,6 +293,48 @@ class ListClass {
       return false;
     }
   }
+  
+  function BuildCache() {
+  	global $SMARTY,$DBobject;
+
+  	$sql[0] = "CREATE TABLE IF NOT EXISTS `cache_tbl_listing` (
+		`cache_id` INT(11) NOT NULL AUTO_INCREMENT,
+		`cache_record_id` INT(11) DEFAULT NULL,
+		`cache_url` VARCHAR(255) DEFAULT NULL,
+		`cache_created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+		`cache_modified` DATETIME DEFAULT NULL,
+		`cache_deleted` DATETIME DEFAULT NULL,
+		PRIMARY KEY (`cache_id`)
+		) ENGINE=MYISAM DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;";
+  	$DBobject->wrappedSql($sql[0]);
+  	
+  	$sql[3] = "TRUNCATE cache_tbl_listing;";
+  	$sql[1] = "SELECT DISTINCT(listing_parent_id) FROM tbl_listing WHERE listing_deleted IS NULL";
+  	$res = $DBobject->wrappedSql($sql[1]);
+
+  	$ids = array();
+  	foreach($res as $row){
+  	  $ids[] = "listing_object_id = {$row['listing_parent_id']}";
+  	}
+  	$sql_r = "SELECT listing_object_id AS id, listing_parent_id,listing_url, listing_published FROM tbl_listing WHERE (".implode(" OR ",$ids).") AND listing_deleted IS NULL GROUP BY CONCAT(listing_object_id,':',listing_url) ORDER BY listing_published = 1"; // GET DISTINCT object_id + url
+  	$res = $DBobject->wrappedSql($sql_r);
+  	foreach($res as $row){
+  	  $id = $row['id'];
+  	  $url = "";
+  	  $published = $row['listing_published'];
+  	  if(! self::BuildUrl($row['id'],$url,$published)){
+  	    continue;
+  	  }
+  	  $sql_c[] = "INSERT INTO cache_tbl_listing (cache_record_id,cache_published,cache_url,cache_modified) SELECT listing_object_id,listing_published, CONCAT('{$url}','/',listing_url), now() FROM tbl_listing WHERE listing_parent_id = {$id} AND listing_deleted IS NULL";
+  	}
+  	$sql_c[] = "INSERT INTO cache_tbl_listing (cache_record_id,cache_published,cache_url,cache_modified) SELECT listing_object_id,listing_published, listing_url, now() FROM tbl_listing WHERE listing_parent_id = 0 AND listing_deleted IS NULL";
+
+  	$DBobject->wrappedSql($sql[3]);
+  	foreach($sql_c as $s){
+  	  $DBobject->wrappedSql($s);
+  	}
+  }
+
   
   // FOR REFERENCE
   /*
--- sitemap.php
+++ sitemap.php
@@ -9,18 +9,22 @@ $SMARTY->caching = false;
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 $domain = $protocol.$_SERVER['HTTP_HOST'];
 
-$sql = "SELECT CONCAT('{$domain}/',cache_url) AS cache_url, cache_modified FROM cache_tbl_listing WHERE cache_deleted IS NULL AND cache_published = '1' ORDER BY cache_url ASC";
-$res = $DBobject->wrappedSql($sql);
-$result = array();
-foreach ($res as $r){
-	if(!array_key_exists($r['cache_url'], $result)) $result["{$r['cache_url']}"] = $r;
+try {
+	$sql = "SELECT CONCAT('{$domain}/',cache_url) AS cache_url, cache_modified FROM cache_tbl_listing LEFT JOIN tbl_listing ON listing_object_id = cache_record_id WHERE cache_deleted IS NULL AND cache_published = '1' AND listing_published = 1 AND listing_deleted IS NULL ORDER BY cache_url ASC";
+	$res = $DBobject->wrappedSql($sql);
+	$result = array();
+	foreach ($res as $r){
+		if(!array_key_exists($r['cache_url'], $result)) $result["{$r['cache_url']}"] = $r;
+	}
+	
+	$sql = "SELECT CONCAT('{$domain}/',cache_url) AS cache_url, cache_modified FROM cache_tbl_product LEFT JOIN tbl_product ON product_object_id = cache_record_id WHERE cache_deleted IS NULL AND cache_published = '1' AND product_published = 1 AND product_deleted IS NULL ORDER BY cache_url ASC";
+	$res = $DBobject->wrappedSql($sql);
+	foreach ($res as $r){
+		if(!array_key_exists($r['cache_url'], $result)) $result["{$r['cache_url']}"] = $r;
+	}
+} catch (Exception $e) {
 }
 
-/* $sql = "SELECT CONCAT('{$domain}/',cache_url) AS cache_url, cache_modified FROM cache_tbl_product WHERE cache_deleted IS NULL AND cache_published = '1' ORDER BY cache_url ASC";
-$res = $DBobject->wrappedSql($sql);
-foreach ($res as $r){
-	if(!array_key_exists($r['cache_url'], $result)) $result["{$r['cache_url']}"] = $r;
-} */
 $SMARTY->assign("pages",$result);
 
 header('Content-Type: application/xml; charset=utf-8');
