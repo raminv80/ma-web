b4ace3abf608237759e9325534a3147336631275
Bikram <bikram@them.com.au>
2018-04-17 15:58:38 +0930

    Bug fix for pushing renewal payments cron
    Signed-off-by: Bikram <bikram@them.com.au>

--- admin/includes/cron/push_renewal_payments.php
+++ admin/includes/cron/push_renewal_payments.php
@@ -14,7 +14,7 @@ $sql = "SELECT tbl_payment.*, tbl_cartitem.cartitem_variant_name, tbl_log.*
   FROM tbl_payment 
   LEFT JOIN tbl_cartitem ON tbl_payment.payment_cart_id = tbl_cartitem.cartitem_cart_id 
   LEFT JOIN tbl_log ON tbl_payment.payment_id = tbl_log.log_record_id AND tbl_log.log_action = 'member-process-payment' AND tbl_log.log_deleted IS NULL 
-  WHERE tbl_payment.payment_is_renewal = '1' AND tbl_payment.payment_deleted IS NULL AND tbl_log.log_id IS NULL";
+  WHERE tbl_payment.payment_is_renewal = '1' AND tbl_payment.payment_status = 'A' AND tbl_payment.payment_deleted IS NULL AND tbl_log.log_id IS NULL";
 
 if($res = $DBobject->wrappedSql($sql)){
   foreach($res as $record){
