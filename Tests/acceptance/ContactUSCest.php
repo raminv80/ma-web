967af80aabb9ba8daf9c7109b239cf127223aacf
Ramin Vakilian <ramin@them.com.au>
2018-12-03 11:48:31 +1030

    updates contact us test

--- Tests/acceptance/ContactUSCest.php
+++ Tests/acceptance/ContactUSCest.php
@@ -10,13 +10,17 @@ class ContactUSCest
     // tests
     public function contactUsFormSubmits(AcceptanceTester $I)
     {
+        $I->scrollTo('#contact_form');
         $I->fillField('name', ' Acceptance Tester');
         $I->fillField('email', 'acceptancetester@them.com.au');
         $I->fillField('phone', '04511111111');
         $I->fillField('postcode', '5000');
-        $I->fillField('phone', '04001111111');
         $I->fillField('membership_no', '1234');
-        $I->fillField('enquiry', 'THIS IS A TEST');
+        $I->selectOptionInSelectBoxItDropDown('nature_enquiry', 2);
+        $I->fillField('#enquiry', 'THIS IS A TEST');
+        $I->wait(3);//to pass spam prevention
+        $I->click('#fbsub');
+        $I->wait(1);
         $I->see('Thank you for contacting us.');
     }
 }
