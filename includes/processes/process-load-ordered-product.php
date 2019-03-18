e74d4de85757e8d5a4c1054047f213710e0e23ea
Ramin Vakilian <ramin@them.com.au>
2019-03-18 13:32:00 +1030

    Debug: exception invalid arg supplied to foreach
    Case https://sentry.io/organizations/them-advertising/issues/939016490/?project=1323426&query=is%3Aunresolved&statsPeriod=14d&utc=false

--- includes/processes/process-load-ordered-product.php
+++ includes/processes/process-load-ordered-product.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 global $CONFIG, $SMARTY, $DBobject, $GA_ID, $REQUEST_URI;
 try{
   $cartItemId = 0;
@@ -8,7 +8,7 @@ try{
   $is_user_product = false;
   $chk_product = "SELECT cartitem_id FROM tbl_cartitem LEFT JOIN tbl_payment ON cartitem_cart_id = payment_cart_id WHERE payment_user_id = :user_id AND payment_response_code = 'approved'";
   $purchased_products = $DBobject->wrappedSql($chk_product, array(":user_id" => $_SESSION['user']['public']['id']));
-  foreach($purchased_products as $p){
+  if($purchased_products) foreach($purchased_products as $p){
     if($p['cartitem_id'] == $cartItemId){
       $is_user_product = true;
     }
@@ -16,11 +16,11 @@ try{
   $products = array();
   $cart_obj = new cart($_SESSION['user']['public']['id']);
   $ordered_item = array();
-  
+
   if($is_user_product){
     //LOAD PRODUCTS
     $prodObj = new ProductClass('', $CONFIG->product_page);
-    
+
     $sql = "SELECT * FROM tbl_cartitem LEFT JOIN tbl_product ON product_object_id = cartitem_product_id
           LEFT JOIN tbl_variant ON variant_id = cartitem_variant_id
         WHERE (cartitem_deleted IS NULL OR cartitem_deleted = '0000-00-00') 
@@ -41,7 +41,7 @@ try{
     }
   }
   $SMARTY->assign('ordered_item', $ordered_item);
-  
+
 }catch(exceptionCart $e) {
   $SMARTY->assign('error', $e->getMessage());
 }
